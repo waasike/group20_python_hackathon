80753079448bafbd7e601b9ff10fa75eb517d6f7
waasike <mitchellewasikee@gmail.com>
2022-07-09 09:40:02 +0300

    modified commit

--- task2_day2.py
+++ task2_day2.py
@@ -1,1 +1,4 @@
 # CHALLENGE - Task Two (Day 2)
+carrier_option = ["Computer Science", "Data Science", "Software Engineering", "Web Development", "Network Security"]
+carrier_advices = [input("Any career questions?")]
+carrier_questions = [input("Any career questions?")]
